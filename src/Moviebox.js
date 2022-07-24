aacb563b3a9addda56c62cd8111d706e2a74d6e7
Divya Jain <divya.jain@cgi.com>
2022-07-24 19:48:07 +0530

    warning fixed

--- src/Moviebox.js
+++ src/Moviebox.js
@@ -12,7 +12,7 @@ const MovieBox =({titel, poster_path, vote_average, release_date, overview})=>{
     return(
         <div className="card text-center bg-secondary mb-3">
             <div className="card-body">
-              <img className="card-img-top" src={API_IMG+poster_path} />
+              <img className="card-img-top" src={API_IMG+poster_path} alt="img" />
               <div className="card-body">
                   <button type="button" className="btn btn-dark" onClick={handleShow} >View More</button>
                   <Modal show={show} onHide={handleClose}>
